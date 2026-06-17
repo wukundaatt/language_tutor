e0ad20701b5cc739558def2d3cf5ec5619eb5e0a
wukundaatt <wukundaatt@users.noreply.github.com>
2026-06-17 05:02:35 +0000

    feat: 启动服务
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "run-agent-6a31510c1a06ff6df71a45bf-mqh8ff33.remote-agent.svc.cluster.local",
+    "run-agent-6a32259df0687f83c5a0e394-mqhl3tfy-preview.agent-sandbox-bj-d3-gw.trae.cn",
+    "127.0.0.1",
+    "localhost",
   ],
 };
 
